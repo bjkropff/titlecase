9cf71192923a244c1b33752289e86e4f0dfc56d0
Brian <Brian.Kropff@gmail.com>
2015-07-06 15:44:43 -0700

    Add code to pass first test

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -1,5 +1,24 @@
 <?php
-    
+    require_once "src/TitleCaseGenerator.php";
+
+    class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
+    {
+
+      function test_makeTitleCase_oneWord()
+      {
+        //Arrange
+        $test_TitleCaseGenerator = new TitleCaseGenerator;
+        $input = "beowolf";
+
+        //Act
+        $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+        //Assert
+        $this->assertEquals("Beowolf", $result);
+
+
+      }
+    }
 
 
 
