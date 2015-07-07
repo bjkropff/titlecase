b3dd21931740e2384480c536699571d1f068085b
Brian <Brian.Kropff@gmail.com>
2015-07-06 17:08:25 -0700

    Add README.md and fix naming issue

--- app/app.php
+++ app/app.php
@@ -15,8 +15,8 @@
     $app->get("/view_title_case", function() use($app) {
         $my_TitleCaseGenerator = new TitleCaseGenerator;
         $title_cased_phrase = $my_TitleCaseGenerator->makeTitleCase($_GET['phrase']);
-        return $app['twig']->render('title_cased.twig', array('result' => $title_cased_phrase));
+        return $app['twig']->render('title_cased.html.twig', array('result' => $title_cased_phrase));
     });
 
     return $app;
-    ?>
+?>
