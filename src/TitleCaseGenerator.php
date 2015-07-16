b39f232f9d52ef3dd7cc54a934f76024a18f1649
Brian <Brian.Kropff@gmail.com>
2015-07-15 19:09:39 -0700

    Add code to make mutiple words non titlecase (i.e. 'of', 'for'). This will break the second test.

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -9,14 +9,17 @@
             //Declares a new, empty array.
             $output_titlecased = array();
 
+            //Declares a new array of many non-titlecased words.
+            $compares = array("a", "an", "and", "as", "at", "but", "by", "for", "from", "in", "nor", "of", "on", "or", "the", "to", "up");
+
             //Runs a foreach on $input_array_of_words
             foreach ($input_array_of_words as $word) {
-                //if word is of, push it to the array
-                if($word == "of"){
+                //if word is in the array $compares, push it to the array
+                if(in_array($word, $compares)){
                     array_push($output_titlecased, $word);
                 }
-                //if word is NOT of, capitalize it and push it to the array
-                if($word !== "of"){
+                //if word is NOT in the array $compares, capitalize it and push it to the array
+                if(!in_array($word, $compares)){
                     array_push($output_titlecased, ucfirst($word));
                 }
             }
