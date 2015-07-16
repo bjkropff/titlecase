23a9ec49543fb6e377c1094fa4007dfe5b1cbb8a
Brian <Brian.Kropff@gmail.com>
2015-07-15 18:56:21 -0700

    Add test for multiple words to NOT capitalize

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -35,7 +35,7 @@
 
       }
 
-      // Test to NOT capitalize chosen words in a title
+      // Test to NOT capitalize chosen word ('of') in a title
       function test_makeTitleCase_nonChosenWord()
       {
         //Arrange
@@ -50,6 +50,38 @@
 
 
       }
+      // Test to NOT capitalize chosen words in a title
+      function test_makeTitleCase_multipleNotChosen()
+      {
+        //Arrange
+        $test_TitleCaseGenerator = new TitleCaseGenerator;
+        $input = "father of the bride";
+
+
+        //Act
+        $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+        //Assert
+        $this->assertEquals("Father of the Bride", $result);
+
+
+      }
+      // // Test to NOT capitalize chosen words in a title unless it is the first word
+      // function test_makeTitleCase_nonChosenWord()
+      // {
+      //   //Arrange
+      //   $test_TitleCaseGenerator = new TitleCaseGenerator;
+      //   $input = "the little shop of horrors";
+      //
+      //
+      //   //Act
+      //   $result = $test_TitleCaseGenerator->makeTitleCase($input);
+      //
+      //   //Assert
+      //   $this->assertEquals("Little Shop of Horrors", $result);
+      //
+      //
+      // }
     }
 
 
