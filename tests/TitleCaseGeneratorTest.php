70713dbb6339ee723d079b55ad1db3dcda83add0
Brian <Brian.Kropff@gmail.com>
2015-07-15 18:42:37 -0700

    Add test for a word  to not capitalize (e.g. 'of')

--- tests/TitleCaseGeneratorTest.php
+++ tests/TitleCaseGeneratorTest.php
@@ -3,7 +3,7 @@
 
     class TitleCaseGeneratorTest extends PHPUnit_Framework_TestCase
     {
-
+      // Basic test to capitalize a one word title
       function test_makeTitleCase_oneWord()
       {
         //Arrange
@@ -19,6 +19,7 @@
 
       }
 
+      // Test to capitalize a multiple word title
       function test_makeTitleCase_multipleWord()
       {
         //Arrange
@@ -30,6 +31,22 @@
 
         //Assert
         $this->assertEquals("The Little Mermaid", $result);
+
+
+      }
+
+      // Test to NOT capitalize chosen words in a title
+      function test_makeTitleCase_nonChosenWord()
+      {
+        //Arrange
+        $test_TitleCaseGenerator = new TitleCaseGenerator;
+        $input = "little shop of horrors";
+
+        //Act
+        $result = $test_TitleCaseGenerator->makeTitleCase($input);
+
+        //Assert
+        $this->assertEquals("little shop of horrors", $result);
 
 
       }
