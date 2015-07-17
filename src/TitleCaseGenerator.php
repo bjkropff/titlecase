24b8f3296432a4e45951a1c838a09fa2e49796c5
Brian <Brian.Kropff@gmail.com>
2015-07-17 11:11:22 -0700

    Add commits on future fixs

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -6,6 +6,9 @@
             //This 'explodes' the $input_title into an array of strings.
             $input_array_of_words = explode(" ", strtolower($input_title));
 
+            //Add counter of the length of the array for comparing in foreach loop
+            //$array_length_for_counting = count($input_array_of_words);
+
             //Declares a new, empty array.
             $output_titlecased = array();
 
@@ -21,6 +24,8 @@
                 //if word is in the array $compares, push it to the array
                 }elseif ((in_array($word, $compares)) && ($output_titlecased != array()))
                 {
+                    //Need to find if it is the last element in the title. True -> capitalize, false -> leave lowercase
+                    //if($array_length_for_counting == count($output_titlecased))
                     array_push($output_titlecased, $word);
                 }
             }
