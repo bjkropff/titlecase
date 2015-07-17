29e345f3e5d30dd1718798bd1e07bc88fab55918
Brian <Brian.Kropff@gmail.com>
2015-07-17 11:04:58 -0700

    Add code to pass string to lowercase

--- src/TitleCaseGenerator.php
+++ src/TitleCaseGenerator.php
@@ -4,7 +4,7 @@
         function makeTitleCase($input_title)
         {
             //This 'explodes' the $input_title into an array of strings.
-            $input_array_of_words = explode(" ", $input_title);
+            $input_array_of_words = explode(" ", strtolower($input_title));
 
             //Declares a new, empty array.
             $output_titlecased = array();
